eb34f6e997aa912564734a85859d36d25d2cd29e
opilane <opilane@HP-CZC6297BBL>
2020-11-18 09:57:13 +0200

    Create Ajaplaan

--- AjaplaanOsa3/AjaplaanOsa3/MainPage.xaml.cs
+++ AjaplaanOsa3/AjaplaanOsa3/MainPage.xaml.cs
@@ -23,61 +23,73 @@ namespace AjaplaanOsa3
 
             if (_triggerTime.Hours.ToString() == "1")
             {
+                //Food Time"
                 LabelInfo.Text = "Food Time";
                 imageforpicker.Source = "Food.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "2")
             {
+                //School Time
                 LabelInfo.Text = "School Time";
                 imageforpicker.Source = "School.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "3")
             {
+                //Lunch Time
                 LabelInfo.Text = "Lunch Time";
                 imageforpicker.Source = "Lunch.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "4")
             {
+                //Film Time
                 LabelInfo.Text = "Film Time";
                 imageforpicker.Source = "Film.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "5")
             {
+                //Street Time
                 LabelInfo.Text = "Street Time";
                 imageforpicker.Source = "Sreet.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "6")
             {
+                //Cleaning Time
                 LabelInfo.Text = "Cleaning Time";
                 imageforpicker.Source = "Cleaning.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "7")
             {
+                //Lessons Time
                 LabelInfo.Text = "Lessons Time";
                 imageforpicker.Source = "Lessons.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "8")
             {
+                //Dinner Time
                 LabelInfo.Text = "Dinner Time";
                 imageforpicker.Source = "Dinner.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "9")
             {
+                //Recreation Time
                 LabelInfo.Text = "Recreation Time";
                 imageforpicker.Source = "Recreation.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "10")
             {
+                //Recreation Time
                 LabelInfo.Text = "Bath Time";
                 imageforpicker.Source = "Bath.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "11")
             {
+                //Tv Time
                 LabelInfo.Text = "Tv Time";
                 imageforpicker.Source = "Tv.jpg";
             }
             else if (_triggerTime.Hours.ToString() == "0")
             {
+                //Sleep
                 LabelInfo.Text = "Sleep";
                 imageforpicker.Source = "Sleep.jpg";
             }
